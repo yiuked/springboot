8111b893c2818597ce684b859e865552b88a6747
Company <yiuked@vip.qq.com>
2018-10-10 17:01:43 +0800

    init

---
+++ src/main/java/com/example/server/ReadingListRepository.java
@@ -0,0 +1,8 @@
+package com.example.server;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ReadingListRepository extends JpaRepository<Book,Long>{
+    List<Book> findByReader(String reader);
+}
